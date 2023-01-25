8a2729d60706e62189a7f661c683d594122b641c
parkhyejin <qhj1019@naver.com>
2023-01-26 01:59:50 +0900

    type animation 추가

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import style from './Main.module.css';
 import { motion } from "framer-motion";
+import Typewriter from "typewriter-effect";
 
 const Main = (props) => {
 	return (
@@ -11,7 +12,21 @@ const Main = (props) => {
             className={`pageArea ${style.main}` }
         >
             <div className={style.mainTxt}>
-                <h2>안녕하세요.<br/>신입 웹 개발자 <span>박혜진</span>입니다.</h2>
+                <h2>
+                    <Typewriter
+                        className={style.typeTxt}
+                        onInit={(typewriter) => {
+                            typewriter
+                            .typeString('안녕하세요.')
+                            .pauseFor(100)
+                            .typeString('<br/>신입 웹 개발자 &nbsp;')
+                            .pauseFor(100)
+                            .typeString('<b>박혜진</b> 입니다.')
+                            .start()
+                            .pauseFor(100)
+                        }}
+                    />
+                </h2>
                 <h3>
                     "성공이라는 못을 박으려면 끈질김이라는 망치가 필요하다"<br className="pc"/>
                     저의 장점은 꾸준함과 차분함 입니다. 꾸준히 노력하는 개발자가 되겠습니다.
